91fbe2bf0beb42a2e4ab690372a6e0189e2698e0
Shubh Agarwal <f20212770@goa.bits-pilani.ac.in>
2023-07-05 14:05:46 +0530

    1

--- upload.php
+++ upload.php
@@ -22,23 +22,3 @@
 
     }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload</title>
-</head>
-<body>
-    <img src="uploads/pic.png" alt="Image Description">
-    <form action="?" method = "POST" enctype = "multipart/form-data">
-    <label>Upload Files</label>
-    <p ><input type="file" name="file"/></p>
-    <p><input type="submit" name ="upload" value="Upload Image"></p>
-</form>
-
-</body>
-</html>
